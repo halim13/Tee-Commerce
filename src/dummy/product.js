7a51b0fad3ac9064c8498292b2bf255d41bc3fc6
Halim <hasanudinhalim@gmail.com>
2023-05-30 17:15:28 +0700

    npm

---
+++ src/dummy/product.js
@@ -0,0 +1,173 @@
+export const product = [
+  {
+    nama: 'Kaus Satu',
+    harga: 78000,
+    kategori: [
+      {
+        name: 'katun'
+      },
+      {
+        name: 'kaus'
+      },
+      {
+        name: 'lengan pendek'
+      }
+    ],
+    stok: 20,
+    warna: 'merah',
+    created_date: '2023-05-03',
+    id: '1'
+  },
+  {
+    nama: 'Kemeja Satu',
+    harga: 160000,
+    kategori: [
+      {
+        name: 'katun'
+      },
+      {
+        name: 'kemeja'
+      },
+      {
+        name: 'lengan panjang'
+      }
+    ],
+    stok: 30,
+    warna: 'hitam',
+    created_date: '2023-05-11',
+    id: '2'
+  },
+  {
+    nama: 'Kemeja Dua',
+    harga: 190000,
+    kategori: [
+      {
+        name: 'chambray'
+      },
+      {
+        name: 'kemeja'
+      },
+      {
+        name: 'lengan panjang'
+      }
+    ],
+    stok: 11,
+    warna: 'biru',
+    created_date: '2023-05-08',
+    id: '3'
+  },
+  {
+    nama: 'Kaus Dua',
+    harga: 50000,
+    kategori: [
+      {
+        name: 'denim'
+      },
+      {
+        name: 'kaus'
+      },
+      {
+        name: 'lengan pendek'
+      }
+    ],
+    stok: 45,
+    warna: 'kuning',
+    created_date: '2023-05-13',
+    id: '4'
+  },
+  {
+    nama: 'Kaus Tiga',
+    harga: 52000,
+    kategori: [
+      {
+        name: 'baby canvas'
+      },
+      {
+        name: 'kaus'
+      },
+      {
+        name: 'Lengan Pendek'
+      }
+    ],
+    stok: 13,
+    warna: 'cream',
+    created_date: '2023-05-14',
+    id: '5'
+  },
+  {
+    nama: 'Kaus Empat',
+    harga: 56000,
+    kategori: [
+      {
+        name: 'drill'
+      },
+      {
+        name: 'kaus'
+      },
+      {
+        name: 'lengan panjang'
+      }
+    ],
+    stok: 53,
+    warna: 'abu abu',
+    created_date: '2023-05-14',
+    id: '6'
+  },
+  {
+    nama: 'Celana Satu',
+    harga: 200000,
+    kategori: [
+      {
+        name: 'denim'
+      },
+      {
+        name: 'celana'
+      },
+      {
+        name: 'celana panjang'
+      }
+    ],
+    stok: 49,
+    warna: 'Hitam',
+    created_date: '2023-05-20',
+    id: '7'
+  },
+  {
+    nama: 'Celana Dua',
+    harga: 175000,
+    kategori: [
+      {
+        name: 'kanvas'
+      },
+      {
+        name: 'celana'
+      },
+      {
+        name: 'celana pendek'
+      }
+    ],
+    stok: 57,
+    warna: 'biru',
+    created_date: '2023-05-21',
+    id: '8'
+  },
+  {
+    nama: 'Celana Tiga',
+    harga: 210000,
+    kategori: [
+      {
+        name: 'lotto'
+      },
+      {
+        name: 'celana'
+      },
+      {
+        name: 'celana panjang'
+      }
+    ],
+    stok: 34,
+    warna: 'cream',
+    created_date: '2023-05-21',
+    id: '9'
+  }
+]
